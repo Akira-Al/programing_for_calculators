2aaf1e341d4a1ea87f2d749f00b484fd388a1ca5
arrow65 <78029315+arrows65@users.noreply.github.com>
2024-06-25 14:58:43 +0900

    辞書読み込みのテストを綺麗に

--- tests/test_load_dictionary.py
+++ tests/test_load_dictionary.py
@@ -1,10 +1,5 @@
-import pytest
-import os
-import sys
-
-project_root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(project_root_path)
-import src.my_library.load_dictionary as dictionary_loader
+from ..src.my_library import load_dictionary
+from pathlib import Path
 
 
 def test_load_dictionary1():
@@ -12,8 +7,8 @@ def test_load_dictionary1():
     dictionary_loaderモジュール内のload_dict1関数のテスト
     dictionary.txtと同様の形式のファイルから辞書が正しくloadされるかチェックする
     """
-    input_file = os.path.join(
-        project_root_path, "tests", "test_load_dictionary_files", "test_dictionary1.txt"
+    input_file = (
+        Path(__file__).parent / "test_load_dictionary_files" / "test_dictionary1.txt"
     )
     expected_output = {
         "２，３日": (0, "〜である・になる（状態）客観"),
@@ -22,7 +17,7 @@ def test_load_dictionary1():
         "25%": (0, "〜である・になる（状態）客観"),
         "80%": (0, "〜である・になる（状態）客観"),
     }
-    assert dictionary_loader.load_dict1(input_file) == expected_output
+    assert load_dictionary.load_dict1(input_file) == expected_output
 
 
 def test_load_dictionary2():
@@ -30,8 +25,8 @@ def test_load_dictionary2():
     dictionary_loaderモジュール内のload_dict2関数のテスト
     dictionary2.txtと同様の形式のファイルから辞書が正しくloadされるかチェックする
     """
-    input_file = os.path.join(
-        project_root_path, "tests", "test_load_dictionary_files", "test_dictionary2.txt"
+    input_file = (
+        Path(__file__).parent / "test_load_dictionary_files" / "test_dictionary2.txt"
     )
     expected_output = {
         "あがく": [(["あがく"], -1, "経験")],
@@ -39,7 +34,7 @@ def test_load_dictionary2():
         "あきる": [(["あきる"], -1, "経験")],
         "あきれる": [(["あきれる"], -1, "経験"), (["あきれる", "た"], -1, "経験")],
     }
-    assert dictionary_loader.load_dict2(input_file) == expected_output
+    assert load_dictionary.load_dict2(input_file) == expected_output
 
 
 print(test_load_dictionary2())
@@ -50,9 +45,7 @@ def test_load_PnJa():
     dictionary_loaderモジュール内のload_PnJa関数のテスト
     pn_ja.txtと同様の形式のファイルから辞書が正しくloadされるかチェックする
     """
-    input_file = os.path.join(
-        project_root_path, "tests", "test_load_dictionary_files", "test_pn_ja.txt"
-    )
+    input_file = Path(__file__).parent / "test_load_dictionary_files" / "test_pn_ja.txt"
     expected_output = {
         "優れる": (1.0, "すぐれる", "動詞"),
         "良い": (0.999995, "よい", "形容詞"),
@@ -60,4 +53,4 @@ def test_load_PnJa():
         "褒める": (0.999979, "ほめる", "動詞"),
         "めでたい": (0.999645, "めでたい", "形容詞"),
     }
-    assert dictionary_loader.load_PnJa(input_file) == expected_output
+    assert load_dictionary.load_PnJa(input_file) == expected_output
