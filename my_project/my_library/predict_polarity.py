8628ecc09906061cab94b7530db7b9618a640ea8
Akira-Al <tonozukaalexanderakira@gmail.com>
2024-06-25 21:58:05 +0900

    debug_落ち込む

--- my_project/my_library/predict_polarity.py
+++ my_project/my_library/predict_polarity.py
@@ -15,35 +15,53 @@ class PolarEstimator:
         self.tokenizer = Tokenizer()
 
     def estimate_v1(self, text: str, *, verbose=False) -> int:
+        debug_text = ""
         if verbose:
-            print("text:", text)
+            debug_text+="text: "+text+"\n"
         score = 0
         for token in self.tokenizer.tokenize(text):
             if token.base_form in self.dict1:
                 s = self.dict1[token.base_form][0]
                 if verbose:
-                    print("[dict1] ", token.base_form, s)
+                    debug_text+="[dict1] "+str(token.base_form)+" "+ str(s)+"\n"
                 score += s
-        return score
+        if verbose:
+            return score, debug_text
+        else:
+            return score
 
     def estimate_v2(self, text: str, *, verbose=False) -> int:
-        score = self.estimate_v1(text, verbose=verbose)
+        debug_text=""
+        score ,d_text= self.estimate_v1(text, verbose=verbose)
 
+        debug_text+=d_text
         is_pending = False
         pending = []
         for token in self.tokenizer.tokenize(text):
+            debug_text+=str(token)+"\n"
+            flag=False
+            if(token.base_form =="落ち込む"):
+                print("||||||||||||")
+                flag=True
             if token.base_form in self.dict2:
+                if flag:
+                    print("#####")
+                    print(token)
                 is_pending = True
 
             if is_pending:
                 pending.append(token.base_form)
 
                 settled = True
+                if(flag):
+                    print(pending)
+                    print(self.dict2[pending[0]])
                 for record in self.dict2[pending[0]]:
+                    debug_text+="[dict2]found "+str(record)+"\n"
                     if pending == record[0]:
                         s = record[1]
                         if verbose:
-                            print("[dict2] ", pending, s)
+                            debug_text+="[dict2] "+str(pending)+str(s)+"\n"
                         score += s
                         break
                     elif partial_match(pending, record[0]):
@@ -52,4 +70,6 @@ class PolarEstimator:
                 if settled:
                     is_pending = False
                     pending = []
+        if verbose:
+            return score, debug_text
         return score
